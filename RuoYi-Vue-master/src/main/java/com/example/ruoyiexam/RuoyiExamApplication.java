b5043500e1c8af0b02e01fcb4713231700ff5a5a
LittlePony <1349068712@qq.com>
2025-07-23 11:45:55 +0800

    111111

---
+++ RuoYi-Vue-master/src/main/java/com/example/ruoyiexam/RuoyiExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.ruoyiexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoyiExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuoyiExamApplication.class, args);
+	}
+
+}
